4ad22418fd7f1a15fc5ed7be15ea3c835a0f73e4
Kuba Ober <kuba@mareimbrium.org>
2018-06-13 01:49:24 -0400

    Refactor the code: Separate the initialization file generation.

--- interface2.cpp
+++ interface2.cpp
@@ -1,171 +1,158 @@
 #include "interface2.h"
+#include <QtCharts>
+using namespace QtCharts;
 
-const double pi = 3.14159265358979323846;
+constexpr double pi = 3.14159265358979323846;
 
-QVector<QPointF> Interface2::ricker(double f, double dt ,double length)
- {
-   size_t N = (length - dt/2.0)/dt;
-   const double discrete_length = (N-1) * dt;
+struct RickerParameters {
+    double f; // Hz
+    double dt; // s
+    double length; // s
+    int polarity;
+};
 
-   QVector<QPointF> w(N);
-   for (size_t i = 0; i < N; ++i)
-   {
-      double t = -discrete_length/2.0 + i*dt;
-      w[i].setX(t);
-      w[i].setY(polarity*((1.0 - 2*pi*pi*f*f*t*t) * exp(-pi*pi*f*f*t*t)));
-   }
-   return w;
- }
+QVector<QPointF> rickerData(const RickerParameters &p)
+{
+    size_t N = (p.length - p.dt/2.0)/p.dt;
+    const double discrete_length = (N-1) * p.dt;
 
-
-QLineSeries* Interface2::rickerSeries(double f) {
-   auto *series = new QLineSeries;
-   series->setName(QStringLiteral("Ricker Wavelet for f=%1").arg(f, 2));
-   series->replace(Interface2::ricker(f, dt, length));
-   return series;
+    QVector<QPointF> w(N);
+    for (size_t i = 0; i < N; ++i)
+    {
+        const double t = -discrete_length/2.0 + i*p.dt;
+        w[i].setX(t);
+        w[i].setY(p.polarity * ((1.0 - 2*pi*pi*p.f*p.f*t*t) * exp(-pi*pi*p.f*p.f*t*t)));
+    }
+    return w;
 }
 
-void Interface2::plot()
+QLineSeries* rickerSeries(const RickerParameters &p)
 {
-    m_view.chart()->removeAllSeries();
-    m_view.chart()->addSeries(rickerSeries(f));
-    m_view.chart()->createDefaultAxes();
-    m_view.setMinimumSize(800, 600);
-    m_view.show();
+    auto *series = new QLineSeries;
+    series->setName(QStringLiteral("Ricker Wavelet for f=%1 Hz").arg(p.f, 2));
+    series->replace(rickerData(p));
+    return series;
+}
+
+struct Sismica1Parameters
+{
+    QString impedance_file = QStringLiteral("Impedancia.txt");
+    int trace_count;
+    int sample_count;
+    double sample_rate; // Hz
+    double wavelet_freq; // Hz
+    int polarity; // +1/-1
+    bool use_noise;
+    bool use_standard_reflectivity;
+};
+
+QByteArray initFileContents(const Sismica1Parameters &sp)
+{
+    static const QString header = QStringLiteral(
+                "////////////////////////////////////////////////////////////////////////////////////\n"
+                "//                  INITIALIZATION FILE FOR SISMICA1 PROGRAM                      //\n"
+                "//                                                                                //\n"
+                "// This is the initialization file, it is not required for running the program    //\n"
+                "// but any change on the parameters MUST be done trough it, otherwise pre-loaded  //\n"
+                "// values will be used.                                                           //\n"
+                "//                                                                                //\n"
+                "//                                                                                //\n"
+                "// It MUST be on the same directory as sismica1.exe                               //\n"
+                "//                                                                                //\n"
+                "// The file generated will be named: traços.txt, it will contain the convolution  //\n"
+                "// results.                                                                       //\n"
+                "//                                                                                //\n"
+                "//                                                                                //\n"
+                "// IMPORTANT  :                                                                   //\n"
+                "// ==========                                                                     //\n"
+                "//                                                                                //\n"
+                "// Do not change the amount of lines in this file, except, if you include new para//\n"
+                "// meters in the source code;                                                     //\n"
+                "// DO NOT change the parameters order;                                            //\n"
+                "// Use dot(.) as decimal separator;                                               //\n"
+                "// There MUST NOT be any blank spaces or any non printable characters.            //\n"
+                "//                                                                                //\n"
+                "//                                                                                //\n"
+                "// The parameters are separated by a new line character, so everything along the  //\n"
+                "// line below this header, will be considered as parameter.                       //\n"
+                "//                                                                                //\n"
+                "//                                                                                //\n"
+                "// Parameters                                                                     //\n"
+                "// ==========                                                                     //\n"
+                "//                                                                                //\n"
+                "// Impedance file name                                                            //\n"
+                "// Number of traces (0 use all the traces on file)                                //\n"
+                "// Trace samples amount                                                           //\n"
+                "// Sampling rate                                                                  //\n"
+                "// Wavelet peak frequency                                                         //\n"
+                "// Polarity                                                                       //\n"
+                "// Flag indicating the use of noise                                               //\n"
+                "// Flag indicating the use of standard reflectivity model                         //\n"
+                "//                                                                                //\n"
+                "//                                                                                //\n"
+                "////////////////////////////////////////////////////////////////////////////////////\n");
+
+    QBuffer buf;
+    buf.open(QIODevice::WriteOnly | QIODevice::Text);
+    QTextStream str(&buf);
+    str
+            << header
+            << sp.impedance_file << endl
+            << sp.trace_count << endl
+            << sp.sample_count << endl
+            << sp.sample_rate / 1000.0 << endl
+            << sp.wavelet_freq << endl
+            << sp.polarity << endl
+            << (sp.use_noise ? 1 : 0) << endl
+            << (sp.use_standard_reflectivity ? 1 : 0) << endl;
+    return buf.data();
 }
 
 Interface2::Interface2(QWidget *parent) :
     QWidget(parent)
 {
     ui.setupUi(this);
+    m_runButton.setText(tr("Run"));
     ui.buttonBox->addButton(&m_runButton, QDialogButtonBox::ApplyRole);
-    connect(&m_runButton, &QPushButton::clicked, this, &Interface2::on_RUN_clicked);
+    connect(&m_runButton, &QPushButton::clicked, this, &Interface2::run);
     connect(ui.buttonBox->button(QDialogButtonBox::Close), &QPushButton::clicked, this, &QWidget::close);
-    m_view.setWindowFlag(Qt::Window);
+    m_view.setWindowFlag(Qt::Window); // the view is a top-level window
 }
 
-void Interface2::on_RUN_clicked()
+void Interface2::run()
 {
+    RickerParameters p;
+    p.length = ui.length->value() / 1000.0;
+    p.polarity = ui.positive->isChecked() ? +1 : -1;
+    p.f = ui.freq->value();
+    p.dt = ui.rate->value() / 1000.0;
 
-    length=ui.length->value() / 1000.0;
-    number_traces = ui.traces->value();
-    trace_samples = ui.samples->value();
-    sampling_rate = ui.rate->value();
-    dt1=ui.rate->value();
-    wavelet_freq = ui.freq->value();
-    f=ui.freq->value();
-    dt=dt1/1000;
+    Sismica1Parameters sp;
+    sp.trace_count = ui.traces->value();
+    sp.sample_count = ui.samples->value();
+    sp.sample_rate = ui.rate->value();
+    sp.wavelet_freq = ui.freq->value();
+    sp.polarity = ui.positive->isChecked() ? +1 : -1;
+    sp.use_noise = ui.noise->isChecked();
+    sp.use_standard_reflectivity = ui.reflectivity->isChecked();
 
-    {
-        if(ui.reflectivity->isChecked())/*refletividade padrao*/
-        {
-         standard_reflec= 1;
-        }
-        else
-        {
-         standard_reflec= 0;
-        }
+    bool saved = false;
+    QSaveFile file{"sismica1.ini"};
+    if (file.open(QIODevice::WriteOnly | QIODevice::Text)) {
+        file.write(initFileContents(sp));
+        saved = file.commit();
     }
 
+    plot(rickerSeries(p));
+    if (!saved)
+        QMessageBox::warning(this, tr("Sismica"), tr("Error: Unable to save the file."));
+}
 
-    {
-        if(ui.noise->isChecked())/*introduzir ruido*/
-        {
-          noise= 1;
-        }
-        else
-        {
-         noise= 0;
-        }
-    }
-
-    {
-        if(ui.negative->isChecked())/*polaridade negativa*/
-        {
-          polarity=-1;
-        }
-
-    }
-
-    {
-        if(ui.positive->isChecked())/*polaridade positiva*/
-        {
-          polarity=1;
-        }
-    }
-
-
-  {
-
-    QString outputFilename = "sismica1.ini";
-    QFile outputFile(outputFilename);
-    outputFile.open(QIODevice::WriteOnly|QIODevice::Text);
-
-    /* Check it opened OK */
-    if(!outputFile.isOpen())
-    {
-        QMessageBox::warning(this,tr("Sismica"),tr("- Error, unable to open sismica1.ini for output"));
-
-    }
-
-    /* Point a QTextStream object at the file */
-    QTextStream outStream(&outputFile);
-
-    /* Write the line to the file */
-    outStream << "////////////////////////////////////////////////////////////////////////////////////"<<endl
-              << "//                  INITIALIZATION FILE FOR SISMICA1 PROGRAM                      //"<<endl
-              << "//                                                                                //"<<endl
-              << "// This is the initialization file, it is not required for running the program    //"<<endl
-              << "// but any change on the parameters MUST be done trough it, otherwise pre-loaded  //"<<endl
-              << "// values will be used.                                                           //"<<endl
-              << "//                                                                                //"<<endl
-              << "//                                                                                //"<<endl
-              << "// It MUST be on the same directory as sismica1.exe                               //"<<endl
-              << "//                                                                                //"<<endl
-              << "// The file generated will be named: traços.txt, it will contain the convolution  //"<<endl
-              << "// results.                                                                       //"<<endl
-              << "//                                                                                //"<<endl
-              << "//                                                                                //"<<endl
-              << "// IMPORTANT  :                                                                   //"<<endl
-              << "// ==========                                                                     //"<<endl
-              << "//                                                                                //"<<endl
-              << "// Do not change the amount of lines in this file, except, if you include new para//"<<endl
-              << "// meters on the source code;                                                     //"<<endl
-              << "// DO NOT change the parameters order;                                            //"<<endl
-              << "// Use dot(.) as decimal separator;                                               //"<<endl
-              << "// There MUST NOT be any blank spaces or any non printable charactere.            //"<<endl
-              << "//                                                                                //"<<endl
-              << "//                                                                                //"<<endl
-              << "// The parameters are separated by a new line character, so everything along the  //"<<endl
-              << "// line below this header, will be considered as parameter.                       //"<<endl
-              << "//                                                                                //"<<endl
-              << "//                                                                                //"<<endl
-              << "// Parameters                                                                     //"<<endl
-              << "// ==========                                                                     //"<<endl
-              << "//                                                                                //"<<endl
-              << "// Impedance file name                                                            //"<<endl
-              << "// Number of traces (0 use all the traces on file)                                //"<<endl
-              << "// Trace samples amount                                                           //"<<endl
-              << "// Sampling rate                                                                  //"<<endl
-              << "// Wavelet peak frequency                                                         //"<<endl
-              << "// Polarity                                                                       //"<<endl
-              << "// Flag indicating the use of noise                                               //"<<endl
-              << "// Flag indicating the use of standard reflectivity model                         //"<<endl
-              << "//                                                                                //"<<endl
-              << "//                                                                                //"<<endl
-              << "///////////////////////////////////////////////////////////////////////////////////"<<endl
-              << "Impedancia.txt"<<"\n"
-              << number_traces<<"\n"
-              << trace_samples<<"\n"
-              << sampling_rate/1000<<"\n"
-              << wavelet_freq<<"\n"
-              << polarity<<"\n"
-              << noise<<"\n"
-              << standard_reflec<<"\n";
-
-    /* Close the file */
-    outputFile.close();
-
- }
-    plot();
+void Interface2::plot(QLineSeries *series)
+{
+    m_view.chart()->removeAllSeries();
+    m_view.chart()->addSeries(series);
+    m_view.chart()->createDefaultAxes();
+    m_view.setMinimumSize(800, 600);
+    m_view.show();
 }
--- interface2.h
+++ interface2.h
@@ -1,44 +1,22 @@
 #ifndef INTERFACE2_H
 #define INTERFACE2_H
-#include <QtCharts>
+#include <QChartView>
+#include <QLineSeries>
+#include <QPushButton>
 #include "ui_interface2.h"
-
-using namespace QtCharts;
-
-namespace Ui {
-class Interface2;
-}
 
 class Interface2 : public QWidget
 {
     Q_OBJECT
-
 public:
     explicit Interface2(QWidget *parent = 0);
 
-    QVector<QPointF> ricker(double f, double dt, double length);
-    QLineSeries* rickerSeries(double f);
-
-    double length;
-    int number_traces;
-    int trace_samples;
-    double sampling_rate;
-    int polarity;
-    double wavelet_freq;
-    bool noise;
-    bool standard_reflec;
-    double f;
-    double dt1;
-    double dt;
-
 private:
     Ui::Interface2 ui;
-    QChartView m_view{this};
-    QPushButton m_runButton{"Run"};
-    void plot();
-
-private slots:
-    void on_RUN_clicked();
+    QPushButton m_runButton;
+    QtCharts::QChartView m_view{this};
+    void run();
+    void plot(QtCharts::QLineSeries *);
 };
 
 #endif // INTERFACE2_H
